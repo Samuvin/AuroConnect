5db4e8e1451021a59fcae88ccc63ed18c8591c9e
admin <samuvin.j@gmail.com>
2024-07-18 21:13:38 +0530

    update

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -2,33 +2,26 @@ import { Box, Flex, Spinner } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import useShowToast from "../hooks/useShowToast";
 import Post from "../components/Post";
-import { useRecoilState, useRecoilValue } from "recoil";
+import { useRecoilState } from "recoil";
 import postsAtom from "../atoms/postsAtom";
 import SuggestedUsers from "../components/SuggestedUsers";
-import Contest from "../components/Contest";
-import feedAtom from "../atoms/feedAtom";
-import axios from "axios";
-import userAtom from "../atoms/userAtom";
 
 const HomePage = () => {
-	const feedsort = useRecoilValue(feedAtom);
 	const [posts, setPosts] = useRecoilState(postsAtom);
 	const [loading, setLoading] = useState(true);
 	const showToast = useShowToast();
-	const currentUser = useRecoilValue(userAtom);
 	useEffect(() => {
 		const getFeedPosts = async () => {
 			setLoading(true);
 			setPosts([]);
 			try {
-				const res = await axios.get("/api/posts/feed", {
-					params: { sort: feedsort, user_id: currentUser._id },
-				});
-				const data = res.data;
+				const res = await fetch("/api/posts/feed");
+				const data = await res.json();
 				if (data.error) {
 					showToast("Error", data.error, "error");
 					return;
 				}
+				console.log(data);
 				setPosts(data);
 			} catch (error) {
 				showToast("Error", error.message, "error");
@@ -41,7 +34,6 @@ const HomePage = () => {
 
 	return (
 		<Flex gap="10" alignItems={"flex-start"}>
-			<Contest />
 			<Box flex={70}>
 				{!loading && posts.length === 0 && (
 					<h1>Follow some users to see the feed</h1>
